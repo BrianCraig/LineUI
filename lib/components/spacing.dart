66624c94ec7ca75a71ccbc9c74aceaace22ca434
Brian Eduardo Craig <briancraigok@gmail.com>
2023-12-20 15:39:05 -0300

    feat `Spacing`: static const spacers

--- lib/components/spacing.dart
+++ lib/components/spacing.dart
@@ -12,19 +12,19 @@ class Spacing extends StatelessWidget {
         dimension: LineTheme.of(context).spacing * ratio,
       );
 
-  static Spacing get twice => const Spacing(
-        ratio: 2,
-      );
+  static const Spacing twice = Spacing(
+    ratio: 2,
+  );
 
-  static Spacing get one => const Spacing(
-        ratio: 1,
-      );
+  static const Spacing one = Spacing(
+    ratio: 1,
+  );
 
-  static Spacing get half => const Spacing(
-        ratio: .5,
-      );
+  static const Spacing half = Spacing(
+    ratio: .5,
+  );
 
-  static Spacing get quarter => const Spacing(
-        ratio: .25,
-      );
+  static const Spacing quarter = Spacing(
+    ratio: .25,
+  );
 }
