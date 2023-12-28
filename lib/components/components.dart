5b1588c1bcfd8b4f06d3b2861fc1cd29ccc44705
Brian Eduardo Craig <briancraigok@gmail.com>
2023-12-28 09:13:03 -0300

    feat `SwitchContainer`: add `SwitchContainerStrategy`

--- lib/components/components.dart
+++ lib/components/components.dart
@@ -5,4 +5,4 @@ export 'line_theme.dart' show LineTheme;
 export 'spacing.dart' show Spacing;
 export 'text.dart' show Text;
 export 'scaffold.dart' show Scaffold;
-export 'switch_container.dart' show SwitchContainer;
+export 'switch_container.dart' show SwitchContainer, SwitchContainerStrategy;
--- lib/components/switch_container.dart
+++ lib/components/switch_container.dart
@@ -1,31 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:test_app/components/line_theme.dart';
 
+enum SwitchContainerStrategy {
+  textIsBackground,
+  primaryBackgroundTextSwitch,
+  secondaryBackgroundTextSwitch,
+}
+
 class SwitchContainer extends StatelessWidget {
   const SwitchContainer({
     super.key,
     required this.child,
+    this.strategy = SwitchContainerStrategy.textIsBackground,
   });
 
   final Widget child;
+  final SwitchContainerStrategy strategy;
 
   @override
   Widget build(BuildContext context) {
     final theme = LineTheme.of(context);
+    final newTheme = switch (strategy) {
+      SwitchContainerStrategy.textIsBackground => LineTheme(
+          textColor: theme.backgroundColor,
+          backgroundColor: theme.textColor,
+          primaryColor: theme.primaryColor,
+          secondaryColor: theme.secondaryColor,
+          accentColor: theme.accentColor,
+          lineWidth: theme.lineWidth,
+          spacing: theme.spacing,
+        ),
+      SwitchContainerStrategy.primaryBackgroundTextSwitch => LineTheme(
+          textColor: theme.backgroundColor,
+          backgroundColor: theme.primaryColor,
+          primaryColor: theme.backgroundColor,
+          secondaryColor: theme.secondaryColor,
+          accentColor: theme.accentColor,
+          lineWidth: theme.lineWidth,
+          spacing: theme.spacing,
+        ),
+      SwitchContainerStrategy.secondaryBackgroundTextSwitch => LineTheme(
+          textColor: theme.backgroundColor,
+          backgroundColor: theme.secondaryColor,
+          primaryColor: theme.primaryColor,
+          secondaryColor: theme.backgroundColor,
+          accentColor: theme.accentColor,
+          lineWidth: theme.lineWidth,
+          spacing: theme.spacing,
+        )
+    };
 
     return LineThemeProvider(
-      theme: LineTheme(
-        textColor: theme.textColor,
-        backgroundColor: theme.primaryColor,
-        primaryColor: theme.backgroundColor,
-        secondaryColor: theme.accentColor,
-        accentColor: theme.secondaryColor,
-        lineWidth: theme.lineWidth,
-        spacing: theme.spacing,
-      ),
+      theme: newTheme,
       child: DecoratedBox(
         decoration: BoxDecoration(
-          color: theme.primaryColor,
+          color: newTheme.backgroundColor,
           borderRadius: BorderRadius.all(
             Radius.circular(theme.lineWidth * 4),
           ),
