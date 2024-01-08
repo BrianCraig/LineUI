9e4801a44e84027018f18c9082b00157e95371ac
Brian Eduardo Craig <briancraigok@gmail.com>
2024-01-08 14:13:44 -0300

    feat `LinearInputScreen`: add sample

--- lib/screens/linear_input_screen.dart
+++ lib/screens/linear_input_screen.dart
@@ -4,8 +4,43 @@ import 'package:line_ui/components/linear_input.dart';
 
 import '../components/components.dart';
 
+extension ToStringMedia on Duration {
+  String toStringMedia() {
+    var microseconds = inMicroseconds;
+    var sign = "";
+    var negative = microseconds < 0;
+
+    var hours = microseconds ~/ Duration.microsecondsPerHour;
+    microseconds = microseconds.remainder(Duration.microsecondsPerHour);
+
+    if (negative) {
+      hours = 0 - hours;
+      microseconds = 0 - microseconds;
+      sign = "-";
+    }
+
+    var minutes = microseconds ~/ Duration.microsecondsPerMinute;
+    microseconds = microseconds.remainder(Duration.microsecondsPerMinute);
+
+    var minutesPadding = minutes < 10 ? "0" : "";
+
+    var seconds = microseconds ~/ Duration.microsecondsPerSecond;
+    microseconds = microseconds.remainder(Duration.microsecondsPerSecond);
+
+    var secondsPadding = seconds < 10 ? "0" : "";
+
+    return "$sign$hours:"
+        "$minutesPadding$minutes:"
+        "$secondsPadding$seconds";
+  }
+}
+
 final basicLinearInputStateProvider = StateProvider<double>(
   (ref) => 0.2,
+);
+
+final songLinearInputStateProvider = StateProvider<double>(
+  (ref) => 12,
 );
 
 class LinearInputScreen extends ConsumerWidget {
@@ -64,6 +99,20 @@ class LinearInputScreen extends ConsumerWidget {
                 },
                 showStart: false,
                 showEnd: false,
+              ),
+            ),
+            Spacing.one,
+            SizedBox(
+              width: 320,
+              child: LinearInput(
+                value: ref.watch(songLinearInputStateProvider),
+                onChange: (value) {
+                  ref.read(songLinearInputStateProvider.notifier).state = value;
+                },
+                from: 0,
+                to: 200,
+                valueToText: (seconds) =>
+                    Duration(seconds: seconds.toInt()).toStringMedia(),
               ),
             ),
           ],
