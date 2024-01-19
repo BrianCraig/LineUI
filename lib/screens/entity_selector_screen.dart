79a5d0eae5c26ea861c6f771a89b97ad336994f1
Brian Eduardo Craig <briancraigok@gmail.com>
2024-01-19 19:10:34 -0300

    feat `SingleSelectScreen`: Mandatory and optional examples

--- lib/screens/entity_selector_screen.dart
+++ lib/screens/entity_selector_screen.dart
@@ -3,8 +3,12 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../components/components.dart';
 
-final singleSelectStateProvider = StateProvider<String?>(
-  (ref) => "fourth",
+final singleSelectMandatoryStateProvider = StateProvider<String>(
+  (ref) => "first",
+);
+
+final singleSelectOptionalStateProvider = StateProvider<String?>(
+  (ref) => null,
 );
 
 const items = [
@@ -24,17 +28,46 @@ class EntitySelectorScreen extends ConsumerWidget {
     return Scaffold(
       title: 'SingleSelector / MultiSelector Component',
       child: Center(
-        child: Column(
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             SizedBox(
               width: 180,
-              child: SingleSelector(
-                items: items,
-                selectedItem: ref.watch(singleSelectStateProvider),
-                onChange: (value) {
-                  ref.read(singleSelectStateProvider.notifier).state = value;
-                },
+              child: Column(
+                children: [
+                  Label.text('Single Selector Mandatory'),
+                  Spacing.half,
+                  SingleSelector(
+                    items: items,
+                    selectedItem: ref.watch(singleSelectMandatoryStateProvider),
+                    onChange: (value) {
+                      if (value != null) {
+                        ref
+                            .read(singleSelectMandatoryStateProvider.notifier)
+                            .state = value;
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Spacing.one,
+            SizedBox(
+              width: 180,
+              child: Column(
+                children: [
+                  Label.text('Single Selector Optional'),
+                  Spacing.half,
+                  SingleSelector(
+                    items: items,
+                    selectedItem: ref.watch(singleSelectOptionalStateProvider),
+                    onChange: (value) {
+                      ref
+                          .read(singleSelectOptionalStateProvider.notifier)
+                          .state = value;
+                    },
+                  ),
+                ],
               ),
             ),
           ],
